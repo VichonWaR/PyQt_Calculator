640cf565a060f3844a2927ecd70334ee302b27d3
Vichon Jward <vichonjward@gmail.com>
2020-04-05 08:13:35 -0700

    Python - PyQt5 GUI basic interface

--- hello.py
+++ hello.py
@@ -1,23 +1,46 @@
-import sys
 
 #import QApplications and the required widgets
+
+import sys
+
 from PyQt5.QtWidgets import QApplication
 from PyQt5.QtWidgets import QLabel
-from PyQt5.QtWidgets import QWidget
+from PyQt5.QtWidgets import QMainWindow
+from PyQt5.QtWidgets import QStatusBar
+from PyQt5.QtWidgets import QToolBar
 
-#create an istance of QApplication
-app = QApplication(sys.argv)
+#creating the main windows for the rest to sit on
 
-#creae an instance of my app's GUI
-window = QWidget()
-window.setWindowTitle('PyQt5 App')
-window.setGeometry(100,100,280,80)
-window.move(60,15)
-helloMsg = QLabel('<h1> Hello Vichon </h1>', parent=window)
-helloMsg.move(60,15)
+class Window(QMainWindow):
+    """Main Window."""
+    def __init__(self, parent=None):
+        """Initializer."""
+        super().__init__(parent)
+        self.setWindowTitle('QMainWindow')
+        self.setCentralWidget(QLabel("I'm the Central Widget"))
+        self._createMenu()
+        self._createToolBar()
+        self._createStatusBar()
 
-# show the GUI
-window.show()
+    def _createMenu(self):
+        """Main dropdown menu"""
+        self.menu = self.menuBar().addMenu("&Menu")
+        self.menu.addAction('&Exit', self.close)
 
-#Run my app's event loop (or main loop)
-sys.exit(app.exec_())
+    def _createToolBar(self):
+        """Dropdown tool bar for user options"""
+        tools = QToolBar()
+        self.addToolBar(tools)
+        tools.addAction('Exit', self.close)
+
+    def _createStatusBar(self):
+        """"Showing status of window"""
+        status = QStatusBar()
+        status.showMessage("I'm the Status Bar")
+        self.setStatusBar(status)
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    win = Window()
+    win.show()
+    sys.exit(app.exec_())
